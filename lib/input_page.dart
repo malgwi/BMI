c6d61a614c29bed201df2817370f08723ad1a424
malgwi <malgwisunday@gmail.com>
2020-08-18 16:03:17 +0100

    slider customization(set state)

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -33,7 +33,9 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     onPress: () {
-                      selectedGender = Gender.male;
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
                     },
                     colour: selectedGender == Gender.male
                         ? kActiveCardColor
@@ -47,7 +49,9 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     onPress: () {
-                      selectedGender = Gender.female;
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
                     },
                     colour: selectedGender == Gender.female
                         ? kActiveCardColor
