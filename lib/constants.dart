fbf445615fffc9f0848f637d264574a4ecc71a35
malgwi <malgwisunday@gmail.com>
2020-08-19 15:05:20 +0100

    UI completed(Result page)

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,12 +8,37 @@ const kBottomContainerColor = Color(0xffeb1555);
 
 // const for text
 const kLabelTextStyle = TextStyle(
-  fontSize: 18.0,
+  fontSize: 20.0,
   color: Color(0xff8d8e98),
 );
 
 // const for Number text
 const kNumberTextStyle = TextStyle(
-  fontSize: 40.0,
+  fontSize: 50.0,
   fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xff24d876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
 );
